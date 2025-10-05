2122a3936c4c458e60f534d0040077518cbba6db
Damian Wiercimak <wiercimakdamian@gmail.com>
2025-10-05 17:50:40 +0100

    wfretr

--- zajecia3/src/main.java
+++ zajecia3/src/main.java
@@ -3,5 +3,9 @@ public class main {
     public static void main(String[] args)
     {
         System.out.println("123456");
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
     }
 }
